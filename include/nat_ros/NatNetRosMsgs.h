522409d0de7481c45b46c59d3ca92fea5b82a683
Waleed Alghilan <w.alghilan@gmail.com>
2023-09-29 13:11:46 +0200

    spring_cleaning

---
+++ include/nat_ros/NatNetRosMsgs.h
@@ -0,0 +1,24 @@
+#ifndef NatNET_ROS_MSGS_H
+#define NatNET_ROS_MSGS_H
+
+// include all the messages in msg folder
+#include <nat_ros/Nat_msg.h>
+
+#include <nat_ros/body.h>
+#include <nat_ros/bodies.h>
+
+#include <nat_ros/marker.h>
+#include <nat_ros/markers.h>
+
+#include <nat_ros/skeleton.h>
+#include <nat_ros/skeletons.h>
+
+#include <nat_ros/channel.h>
+
+#include <nat_ros/forcePlate.h>
+#include <nat_ros/forcePlates.h>
+
+#include <nat_ros/device.h>
+#include <nat_ros/devices.h>
+
+#endif // NatNET_ROS_MSGS_H
---
+++ include/nat_ros/NatServer.h
@@ -0,0 +1,77 @@
+// relies on NatNet Sample client header folder.
+// organized into smaller steps
+
+#ifndef NATSERVER_H
+#define NATSERVER_H
+
+
+#include <inttypes.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <vector>
+
+
+#ifdef _WIN32
+#   include <conio.h>
+#else
+#   include <unistd.h>
+#   include <termios.h>
+#endif
+
+#include <nat_ros/NatNetTypes.h>
+#include <nat_ros/NatNetCAPI.h>
+#include <nat_ros/NatNetClient.h>
+
+
+namespace OptiTrack {
+    class NatServer
+    {
+        public:
+
+
+        NatNetClient* g_pClient = NULL;
+        static const ConnectionType kDefaultConnectionType = ConnectionType_Multicast;
+        sServerDescription g_serverDescription;
+        //std::vector< sNatNetDiscoveredServer > g_discoveredServers;
+        sNatNetClientConnectParams g_connectParams;
+        char g_discoveredMulticastGroupAddr[kNatNetIpv4AddrStrLenMax] = NATNET_DEFAULT_MULTICAST_ADDRESS;
+       
+        
+        
+        bool connected = false;
+        
+        NatServer();
+        ~NatServer();
+
+        void Version();
+
+        void Init();   // starts a server client
+        int SearchAndConnect(); // searches for available servers
+        
+        void Test();
+        void Loop();
+        void ResetClient();
+        void DisconnectClient();
+        void FrameCallback(sFrameOfMocapData *data, void* pUserData);
+        void setDataHandler(void(*newDataHandler)(sFrameOfMocapData* data, void* pUserData));
+        int ConnectClient();// Connects to a spcific client. 
+        
+        private:
+
+    };
+    
+    static int g_analogSamplesPerMocapFrame;
+    static std::vector< sNatNetDiscoveredServer > g_discoveredServers;
+    void DataHandler(sFrameOfMocapData* data, void* pUserData);
+    void MessageHandler( Verbosity msgType, const char* msg );
+    void ServerDiscoveredCallback( const sNatNetDiscoveredServer* pDiscoveredServer, void* pUserContext );
+    
+    #ifndef _WIN32
+    char getch();
+    #endif
+
+}
+
+
+#endif
